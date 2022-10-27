66add36686c1a0d6a0f0aaeb41470db4ae2a8346
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2022-10-27 16:03:04 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+  function formatUser(user) {
+    return user.firstname + " " + user.lastname
+  }
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatUser(user)}</h1>
+    }
+    return <h1>Hello, Stranger</h1>
+  }
+  const user = {
+    firstname: "Danny",
+    lastname: "Rodriguez"
+  }
+  // const element = <h1>Welcome to React Course</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{getGreeting(user)}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
